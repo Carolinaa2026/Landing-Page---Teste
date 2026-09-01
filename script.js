896aaf4380ff18e495c3b30d08ba2aa2d7a047e8
Carolina Soares <carolinareis0401@gmail.com>
2026-09-01 20:58:23 +0000

    Primerio commit do projeto

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const yearElement = document.querySelector("#current-year");
+
+  if (yearElement) {
+    yearElement.textContent = new Date().getFullYear();
+  }
+});
